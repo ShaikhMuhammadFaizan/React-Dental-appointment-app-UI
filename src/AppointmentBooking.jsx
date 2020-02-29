3589dd01d70864564b929b8ae26c21cfc24ff55b
Naveed Aheer <naveedaheer@gmail.com>
2020-02-29 18:28:35 +0500

    Updated UI

--- src/AppointmentBooking.jsx
+++ src/AppointmentBooking.jsx
@@ -4,28 +4,48 @@ import { Container } from 'react-bootstrap';
 import './AppointmentBooking.css';
 
 class Appointmentbooking extends Component {
-    render() {
+
+    targetCustomerSection = () => {
         return (
-            <div className="header color-white-text">
-                <div className="text-left">
-                    <p className="heading-text">The Complete</p>
-                    <p className="heading-text">Dental Examination</p>
-                    <p className="desc-text">A few sentences about your project. How it helps to solve clients problem. It should convinces the cutomers.</p>
-                    <div className="row benefit-area d-flex justify-content-between align-items-center text-center">
-                        <div className="benefit-box">
-                            <p className="sub-heading-text">Benefit No.1 </p>
-                            <p className="sub-desc-text">A few sentences about your project. How it helps to solve clients problem.</p>
-                        </div>
-                        <div className="benefit-box">
-                            <p className="sub-heading-text">Benefit No.1 </p>
-                            <p className="sub-desc-text">A few sentences about your project. How it helps to solve clients problem.</p>
-                        </div>
-                    </div>
-                </div>
+            <div>
+                <h3>
+                    Clear Value for Your Target Customer
+                </h3>
+                <h5>
+                    This is sample text here
+                </h5>
                 <div>
-                    {/* form area */}
+                    
                 </div>
             </div>
+        )
+    }
+
+    render() {
+        return (
+            <Container>
+                <div className="header color-white-text">
+                    <div className="text-left">
+                        <p className="heading-text">The Complete</p>
+                        <p className="heading-text">Dental Examination</p>
+                        <p className="desc-text">A few sentences about your project. How it helps to solve clients problem. It should convinces the cutomers.</p>
+                        <div className="row benefit-area d-flex justify-content-between align-items-center text-center">
+                            <div className="benefit-box">
+                                <p className="sub-heading-text">Benefit No.1 </p>
+                                <p className="sub-desc-text">A few sentences about your project. How it helps to solve clients problem.</p>
+                            </div>
+                            <div className="benefit-box">
+                                <p className="sub-heading-text">Benefit No.1 </p>
+                                <p className="sub-desc-text">A few sentences about your project. How it helps to solve clients problem.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        {/* form area */}
+                    </div>
+                </div>
+                {this.targetCustomerSection()}
+            </Container>
         );
     }
 }
