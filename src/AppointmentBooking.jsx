5e9d02eccda57e682caa6d79afebe4d58397d151
ShaikhMuhammadFaizan <shkfaizan19@gmail.com>
2020-03-02 04:11:58 +0500

    header component

--- src/AppointmentBooking.jsx
+++ src/AppointmentBooking.jsx
@@ -1,29 +1,53 @@
 import React, { Component } from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Form, Button } from 'react-bootstrap';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee, faFlask, faTooth } from '@fortawesome/free-solid-svg-icons'
 
 import './AppointmentBooking.css';
 
 class Appointmentbooking extends Component {
     render() {
         return (
-            <div className="header color-white-text">
-                <div className="text-left">
-                    <p className="heading-text">The Complete</p>
-                    <p className="heading-text">Dental Examination</p>
-                    <p className="desc-text">A few sentences about your project. How it helps to solve clients problem. It should convinces the cutomers.</p>
-                    <div className="row benefit-area d-flex justify-content-between align-items-center text-center">
-                        <div className="benefit-box">
-                            <p className="sub-heading-text">Benefit No.1 </p>
-                            <p className="sub-desc-text">A few sentences about your project. How it helps to solve clients problem.</p>
-                        </div>
-                        <div className="benefit-box">
-                            <p className="sub-heading-text">Benefit No.1 </p>
-                            <p className="sub-desc-text">A few sentences about your project. How it helps to solve clients problem.</p>
+            <div className="header color-white-text d-flex justify-content-center align-items-center">
+                <div className="header-element d-flex justify-content-between align-items-center">
+                    <div className="text-area">
+                        <p className="heading-text">The Complete</p>
+                        <p className="heading-text">Dental Examination</p>
+                        <p className="desc-text">A few sentences about your project. How it helps to solve clients problem. It should convinces the cutomers.</p>
+                        <div className="row benefit-area d-flex justify-content-between align-items-center text-center color-black-text">
+                            <div className="mt-10 benefit-box ">
+                                <FontAwesomeIcon icon={faFlask} size="2x" color="#43a4d7" />
+                                <p className="sub-heading-text">Benefit No.1 </p>
+                                <p className="sub-desc-text">A few sentences about your project. How it helps to solve clients problem.</p>
+                            </div>
+                            <div className="benefit-box ">
+                                <FontAwesomeIcon icon={faFlask} size="2x" color="#43a4d7" />
+
+                                <p className="sub-heading-text">Benefit No.1 </p>
+                                <p className="sub-desc-text">A few sentences about your project. How it helps to solve clients problem.</p>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div>
-                    {/* form area */}
+                    <div className="form-area color-black-text">
+                        <FontAwesomeIcon icon={faTooth} size="3x" color="#43a4d7" /> <br /><br />
+                        <h5 >Book an Appointment</h5><br />
+                        <Form>
+                            <Form.Group controlId="formBasicText">
+                                <Form.Control className="text-box text-center" type="text" placeholder="Name" />
+                            </Form.Group>
+
+                            <Form.Group controlId="formBasicEmail">
+                                <Form.Control className="text-box text-center" type="email" placeholder="Email" />
+                            </Form.Group>
+
+                            <Form.Group controlId="formBasicPassword">
+                                <Form.Control className="text-box text-center" type="password" placeholder="Password" />
+                            </Form.Group>
+
+                            <Button variant="primary" type="submit" className="book-appointment-btn"> Book now </Button>
+                        </Form>
+                    </div>
                 </div>
             </div>
         );
